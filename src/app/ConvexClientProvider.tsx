3d1e20c6ce9f795b2842baf240b164ac2a2e15e8
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2024-07-25 23:02:34 +0530

    fixed bugs

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,17 +1,46 @@
+// "use client";
+
+// import { ClerkProvider, useAuth } from "@clerk/nextjs";
+// import { ConvexProvider, ConvexReactClient } from "convex/react";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
+// import { ReactNode } from "react";
+
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+// console.log("Convex URL", process.env.NEXT_PUBLIC_CONVEX_URL);
+// export function ConvexClientProvider({ children }: { children: ReactNode }) {
+//   return (
+//     <ClerkProvider
+//       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+//     >
+//       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+//         {children}
+//       </ConvexProviderWithClerk>
+//     </ClerkProvider>
+//   );
+// }
+
+
 "use client";
 
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-  <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-    {children}
-  </ConvexProviderWithClerk>
-</ClerkProvider>
-  
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
